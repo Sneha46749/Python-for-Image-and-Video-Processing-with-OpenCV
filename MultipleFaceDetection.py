9de8f3d78f144f24918e3f4b435004898b59f403
Sneha <sneha46749@gmail.com>
2020-08-20 21:07:27 +0530

    Detecting mutiple face out of an image using cv2 in Python

--- MultipleFaceDetection.py
+++ MultipleFaceDetection.py
@@ -2,7 +2,7 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-img = cv2.imread("mutiplefacedetection.jpg")
+img = cv2.imread("multiplefacedetection.jpg")
 gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray_img,
