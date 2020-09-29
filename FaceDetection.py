273c61b1125144426bf803d457f59b1429d02412
Sneha <sneha46749@gmail.com>
2020-09-29 21:16:58 +0530

    Detecting face out of an image using cv2 in Python

--- FaceDetection.py
+++ FaceDetection.py
@@ -3,7 +3,6 @@ import cv2
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 img = cv2.imread("facedetection.jpg")
-
 gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.05, minNeighbors=5)
